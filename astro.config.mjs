ab27276875b13824543523dd8611bab8c4a8bd4a
Alex <alx007dp@gmail.com>
2026-02-25 19:13:38 +0100

    HASTA LOS COJONS

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import tunnel from 'astro-tunnel';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://alx005dp.github.io',
+  base: '/marco-aldany-peluqueria',
+  integrations: [react(), tunnel()]
+});
