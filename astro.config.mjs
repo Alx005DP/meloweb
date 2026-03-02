2d842b850bb50a0581ba0fb3f4d923b240eda540
Alex <alx007dp@gmail.com>
2026-03-02 11:12:33 +0100

    fix routes 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alx005dp.github.io',
   // base: '/marco-aldany-peluqueria',
-  integrations: [react(), tunnel()]
+  integrations: [react()]
 });
